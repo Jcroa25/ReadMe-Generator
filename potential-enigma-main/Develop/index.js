a9b603e060aeb06afbf95a3c6e21e4a7f1abb1eb
JuanCarlos Roa Jr <roajrjuancarlos@hotmail.com>
2023-01-17 12:45:20 -0500

    created function to write README and init app

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -13,8 +13,26 @@ const questions = [
 
     {
         type: "input",
-        message:"What is the Title of your project?",
-        name: "title",
+        message:"What is the description of your project?",
+        name: "description",
+    },
+
+    {
+        type: "input",
+        message:"What are the steps for installation to your project?",
+        name: "installation",
+    },
+
+    {
+        type: "input",
+        message:"How will you use the program for your project?",
+        name: "usage",
+    },
+
+    {
+        type: "input",
+        message:"Who/what are the contributions to ",
+        name: "contributions",
     },
 
     {
@@ -37,33 +55,28 @@ const questions = [
 
     {
         type: "input",
-        message:"What is the Title of your project?",
-        name: "title",
-    },
-
-    {
-        type: "input",
-        message:"What is the Title of your project?",
-        name: "title",
-    },
-
-    {
-        type: "input",
-        message:"What is the Title of your project?",
-        name: "title",
-    },
-
-    {
-        type: "input",
-        message:"What is the Title of your project?",
-        name: "title",
+        message:"Go the extra mile and write tests for your application. Then provide examples on how to run them here.",
+        name: "tests",
     },
 ];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    function writeToFile(fileName, data) {
+        return writeFile(fileName, generateMarkdown(data));
+    }
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .createPromptModule(questios)
+    .then((answers)=>
+    writeFile('README.md', answers)
+    )
+    .then (() =>
+    console.log(err)
+    )
+}
 
 // Function call to initialize app
 init();
