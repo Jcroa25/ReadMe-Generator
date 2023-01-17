48689eea8eecc36acf7cc7bc94033edf2dc2fc88
JuanCarlos Roa Jr <roajrjuancarlos@hotmail.com>
2023-01-17 12:34:01 -0500

    added packages,created array questions for app

--- potential-enigma-main/Develop/index.js
+++ potential-enigma-main/Develop/index.js
@@ -1,8 +1,64 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const {writeFile} = require('fs').promises;
+const generateMarkdown = require('.utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
 
+    {
+        type: "input",
+        message:"What is the Title of your project?",
+        name: "title",
+    },
+
+    {
+        type: "input",
+        message:"What is the Title of your project?",
+        name: "title",
+    },
+
+    {
+        type: "input",
+        message:"What is the Title of your project?",
+        name: "title",
+    },
+
+    {
+        type: "input",
+        message:"What is the Title of your project?",
+        name: "title",
+    },
+
+    {
+        type: "input",
+        message:"What is the Title of your project?",
+        name: "title",
+    },
+
+    {
+        type: "input",
+        message:"What is the Title of your project?",
+        name: "title",
+    },
+
+    {
+        type: "input",
+        message:"What is the Title of your project?",
+        name: "title",
+    },
+
+    {
+        type: "input",
+        message:"What is the Title of your project?",
+        name: "title",
+    },
+];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
